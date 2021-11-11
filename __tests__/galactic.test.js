29382e41b83d31a5cdd9e9a50b589c0f8d769332
Frank-Proulx <frankproulx7@gmail.com>
2021-11-11 10:00:09 -0800

    test and recieve good fail for refactor of AgeChanger constructor to include two new properties, an inputted life expectancy and a default to provide if the user does not input

--- __tests__/galactic.test.js
+++ __tests__/galactic.test.js
@@ -3,27 +3,29 @@ import { AgeChanger } from './../src/js/galactic.js';
 
 describe('AgeChanger', () => {
 
-  test('should create AgeChanger class with a property holding the inputted age', () => {
-    let calculated = new AgeChanger(42);
+  test('should create AgeChanger class with a property holding the inputted age and the inputted life expectancy, with a property for the default life expectancy', () => {
+    let calculated = new AgeChanger(42, 76);
     expect(calculated.age).toEqual(42);
+    expect(calculated.lifeExpect).toEqual(76);
+    expect(calculated.lifeExpectDefault).toEqual(79);    
   });
 
-  test('should create a method that returns the inputted age in mercury years', () => {
+  test('should return the inputted age in mercury years', () => {
     let calculated = new AgeChanger(42);
     expect(calculated.mercury()).toEqual(175);
   });
 
-  test('should create a method that returns the inputted age in venus years', () => {
+  test('should return the inputted age in venus years', () => {
     let calculated = new AgeChanger(42);
     expect(calculated.venus()).toEqual(68);
   });
 
-  test('should create a method that returns the inputted age in mars years', () => {
+  test('should return the inputted age in mars years', () => {
     let calculated = new AgeChanger(42);
     expect(calculated.mars()).toEqual(22);
   });
 
-  test('should create a method that returns the inputted age in jupiter years', () => {
+  test('should return the inputted age in jupiter years', () => {
     let calculated = new AgeChanger(42);
     expect(calculated.jupiter()).toEqual(4);
   });
